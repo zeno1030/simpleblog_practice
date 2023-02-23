926f1de04a12274bcbb42dc15d6a55b1af528182
zeno1030 <zeno1030@naver.com>
2023-02-23 17:20:41 +0900

    Update MemberRepository.kt

--- src/main/kotlin/com/example/simpleblog/domain/member/MemberRepository.kt
+++ src/main/kotlin/com/example/simpleblog/domain/member/MemberRepository.kt
@@ -4,3 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository:JpaRepository<Member, Long> {
 }
+
+interface  MemberCustomRepository{
+
+
+}b
+
+class MemberCustomRepositoryImpl(
+    private val queryFactory:SpringDataQueryFactory,
+): MemberCustomRepository{
+
+    fun findMembers(){
+        queryFactory.listOfQ
+    }
+
+}
